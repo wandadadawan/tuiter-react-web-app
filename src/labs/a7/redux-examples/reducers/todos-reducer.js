5171b8be06809bbcee1da0b134159dee3ea545f0
Wanda Wang <chuwang@wustl.edu>
2022-10-31 14:51:20 -0700

    Completed lab7

--- src/labs/a7/redux-examples/reducers/todos-reducer.js
+++ src/labs/a7/redux-examples/reducers/todos-reducer.js
@@ -13,9 +13,25 @@ const initialTodos = [
   },
 ];
 
-const todoSlice = createSlice({
+const todosSlice = createSlice({
   name: 'todos',
   initialState: initialTodos,
+  reducers: {
+    addTodo(state, action) {
+      state.push({
+        _id: (new Date()).getTime(),
+        do: action.payload.do,
+        done: false
+      });
+    },
+    deleteTodo(state, action) {
+      state.splice(action.payload, 1);
+    },
+    todoDoneToggle(state, action) {
+      const todo = state.find(todo => todo._id === action.payload._id);
+      todo.done = !todo.done;
+    }
+  }
 });
-
-export default todoSlice.reducer;
+export const {addTodo, deleteTodo, todoDoneToggle} = todosSlice.actions;
+export default todosSlice.reducer;
--- src/labs/a7/redux-examples/todos-component.js
+++ src/labs/a7/redux-examples/todos-component.js
@@ -1,14 +1,52 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {addTodo, deleteTodo, todoDoneToggle} from "./reducers/todos-reducer";
 
 const Todos = () => {
   const todos = useSelector(state => state.todos);
+  const [todo, setTodo] = useState({do: ''});
+  const dispath = useDispatch();
+  const toggleTodoDone = (todo) => {
+    dispath(todoDoneToggle(todo));
+  };
+  const deleteTodoClickHandler = (index) => {
+    dispath(deleteTodo(index));
+  };
+  const createTodoClickHandler = () => {
+    dispath(addTodo(todo));
+  };
+  const todoChangeHandler = (event) => {
+    const doValue = event.target.value;
+    const newTodo = {
+      do: doValue
+    };
+    setTodo(newTodo);
+  }
+
   return (
     <>
       <h3>Todos</h3>
       <ul className="list-group">
+        <li className="list-group-item">
+          <button onClick={createTodoClickHandler}
+                  className="btn btn-primary w-25 float-end">
+            Create</button>
+          <input
+            onChange={todoChangeHandler}
+            value={todo.do}
+            className="form-control w-75"/>
+        </li>
         {
-          todos.map(todo =>
-            <li className="list-group-item">
+          todos.map((todo, index) =>
+            <li className="list-group-item" key={todo._id}>
+              <button onClick={() => deleteTodoClickHandler(index)}
+                className="btn btn-danger float-end ms-2">
+                Delete
+              </button>
+              <input type="checkbox"
+                     checked={todo.done}
+                     onChange={() => toggleTodoDone(todo)}
+                     className="me-2"/>
               {todo.do}
             </li>)
         }
