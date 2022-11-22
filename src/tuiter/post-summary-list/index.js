ac92706f60ba443da0573eb36492747696901374
Wanda Wang <chuwang@wustl.edu>
2022-11-22 15:54:45 -0800

    Refactor explore component to use TuitsThunk vs normal state

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,8 +1,12 @@
 import PostSummaryItem from "./post-summary-item";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {findTuitsThunk} from "../../services/tuits-thunks";
 
 const PostSummaryList = () => {
-  const {tuits} = useSelector(state => state.tuitsData)
+  const {tuits, loading} = useSelector(state => state.tuitsData);
+  const dispatch = useDispatch();
+  useEffect(() => {dispatch(findTuitsThunk())}, []);
   return (
       <ul className="list-group">
         {
